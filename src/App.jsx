ff22676ada5020054d7f03757335ef0cafddc610
Kamrulthedev <kamrulthedev@gmail.com>
2023-09-12 00:39:31 +0600

    coer,concects, react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Friends from './Friends'
+
+function App() {
+  return (
+    <>
+      <h1>React Expoloer</h1>
+      <Friends></Friends>
+    </>
+  )
+}
+
+export default App
